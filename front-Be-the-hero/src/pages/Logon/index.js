178aeac041fde4ecad87959477f899a4e10216b6
maewhg <ismaelhugo.signal@gmail.com>
2020-04-06 10:58:32 -0300

    Register page

--- front-Be-the-hero/src/pages/Logon/index.js
+++ front-Be-the-hero/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
 import heroesImg from "../../assets/heroes.png";
@@ -21,10 +22,10 @@ export default function Logon() {
           <button className="button" type="submit">
             Entrar
           </button>
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
